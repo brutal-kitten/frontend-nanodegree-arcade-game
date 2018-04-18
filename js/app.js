f8fed4d9cf697063f2def6c0f742d656e5daa8ae
brutal-kitten <olgaprudyus@gmail.com>
2018-04-18 17:12:29 +0200

    Fix: make background to appear instead of white page

--- js/app.js
+++ js/app.js
@@ -24,13 +24,34 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+  constructor(name){
+    this.name;
+    this.sprite = 'images/char-boy.png';
+  }
+
+  update(dt) {
+
+  };
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+
+  handleInput(direction) {
+
+
+  };
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var allEnemies = [enemy1, enemy2];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
