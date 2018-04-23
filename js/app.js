4a296dfda1457999ce2a3af4742278f3eab3ba06
brutal-kitten <olgaprudyus@gmail.com>
2018-04-23 12:14:28 +0200

    feat: implement the handleInput function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
+const X_STEP = 100;
+const Y_STEP = 90;
+const LEFT_BORDER = 0;
+const RIGHT_BORDER = 450;
+const TOP_BORDER = -50;
+const BOTTOM_BORDER = 490;
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
+  this.x = x;
+  this.y = y;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -32,6 +42,34 @@ class Player {
     this.sprite = 'images/char-boy.png';
   }
 
+  moveLeft () {
+    this.x -= X_STEP;
+    if (this.x < LEFT_BORDER) {
+      this.x = 400;
+    };
+  };
+
+  moveRight () {
+    this.x += X_STEP;
+    if (this.x > RIGHT_BORDER) {
+      this.x = 0;
+    };
+  };
+
+  moveUp () {
+    this.y -= Y_STEP;
+    if (this.y < TOP_BORDER) {
+      this.y = 410;
+    };
+  };
+
+  moveDown () {
+    this.y += Y_STEP;
+    if (this.y > BOTTOM_BORDER) {
+      this.y = -40;
+    };
+  };
+
   update(dt) {
 
   };
@@ -41,7 +79,20 @@ class Player {
   };
 
   handleInput(direction) {
-
+    switch(direction) {
+      case "left":
+        this.moveLeft();
+        break;
+      case "right":
+        this.moveRight();
+        break;
+      case "up":
+        this.moveUp();
+        break;
+      case "down":
+        this.moveDown();
+        break;
+    };
 
   };
 }
@@ -50,10 +101,10 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
+var enemy1 = new Enemy(0, 200);
+var enemy2 = new Enemy(0, 100);
 var allEnemies = [enemy1, enemy2];
-var player = new Player("boy", 200, 400);
+var player = new Player("boy", 200, 410);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
